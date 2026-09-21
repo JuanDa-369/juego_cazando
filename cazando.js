d72a39d2fb4816acc54cb26aadff4e00322387a4
JuanDa-369 <juanjees69@gmail.com>
2026-09-21 10:03:10 -0500

    Se agrega el canvas y la funcion graficarGato

---
+++ cazando.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("areaJuego");
+let ctx = canvas.getContext("2d");
+
+function graficarGato(){
+    ctx.fillStyle="orange";
+    ctx.fillRect(200, 200, 50, 50);
+}
